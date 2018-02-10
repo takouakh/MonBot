c0dc2a39c0467e46d8ab2c2e4d0decaa07fc2f0c
takoua kharroubi <takoua.kh@hotmail.com>
2018-02-10 17:55:42 +0100

    first comm2

--- index.js
+++ index.js
@@ -1,162 +1,159 @@
 
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+'use strict'
+const 
+  express = require('express'),
+  bodyParser = require('body-parser');
+const request = require('request')
+const AIMLInterpreter = require('aimlinterpreter');
+let app = express();
+app.use(bodyParser.urlencoded({"extended": false}));
+app.use(bodyParser.json());
+
+
 
 app.set('port', (process.env.PORT || 5000))
 
-// Process urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+const token = process.env.FB_PAGE_ACCESS_TOKEN
 
-// Process json
-app.use(bodyParser.json())
-
-// Index route
-app.get('/', function (req, res) {
-    res.send('Bonjour, Je suis votre Facebook Messenger Bot')
+app.get('/', function (req, res) { 
+res.send('Dirty Code 12')
 })
 
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'Bot_Messenger_App') {
-        res.send(req.query['hub.challenge'])
+
+app.get('/Contact', function (req, res) { 
+res.send('Contact kharroubi Taha taha.kharroubii@outlook.com')
+})
+
+app.get('/webhook/', function (req, res)
+ { 
+  if (req.query['hub.verify_token'] === 'taha') {  res.send(req.query['hub.challenge']) } else {  res.send('Error, wrong token') }})
+
+app.listen(app.get('port'), function() { 
+console.log('running on port', app.get('port'))
+})
+
+app.post('/webhook', (req, res) => {  
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+      
+      // Get the sender PSID
+  let sender_psid = webhook_event.sender.id;
+  console.log('Sender PSID: ' + sender_psid+'-'+body.last_name);
+
+  // Check if the event is a message or postback and
+  // pass the event to the appropriate handler function
+  if (webhook_event.message) {
+    handleMessage(sender_psid, webhook_event.message);        
+  } else if (webhook_event.postback) {
+    handlePostback(sender_psid, webhook_event.postback);
+  }
+     
+     
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text":  getAiml(received_message.text)
     }
-    res.send('Error, wrong token')
-})
-
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
-
-
-// End Point
-
-app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
-        if (event.message && event.message.text) {
-            text = event.message.text
-            if (text === 'salut') {
-                sendGenericMessage(sender)
-                continue
-            }
-            sendTextMessage(sender, "Bot: " + text.substring(0, 200))
-        }
-        if (event.postback) {
-            text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-            continue
-        }
-    }
-    res.sendStatus(200)
-})
-
-var token = "EAAdpiKzw988BADdgcmK08mwMhdve2QJSzKo8OHAcwG7kskqOSqw9hDZCIC2nUgQnHZBJAksHqeNJjwY3KijfUZBW0l2cZA89zvfxOStHSDWSJYpkdpBieYZA5XXOcnZC2MWOx99LfZATzJgUcH8atO4dlWnajZAJ4RZBhxofZBS4489gZDZD"
-
-// Echo back messages
-
-function sendTextMessage(sender, text) {
-    messageData = {
-        text:text
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+  }  
+  
+  // Sends the response message
+  SendMessage(sender_psid, response);    
 }
 
 
-// Two cards.
-
-function sendGenericMessage(sender) {
-    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Si vous souhaitez suivre",
-                    "subtitle": "social network",
-                    "image_url": "https://raw.githubusercontent.com/mlaidouni/FacebookBot/master/chatbot-facebook.jpg",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.facebook.com/groups/1795624087378868/",
-                        "title": "FB Facebook Group"
-                    }, {
-                        "type": "web_url",
-                        "url": "https://www.youtube.com/channel/UCRV86HkxxSGI-whauOMkQdw",
-                        "title": "Me Suivre sur Youtube"
-                    },{
-                        "type": "web_url",
-                        "url": "https://twitter.com/elmehdimobi",
-                        "title": "Me Suivre sur Twitter"
-                    }],
-                }, {
-                    "title": "Choses à savoir créer un bot",
-                    "subtitle": "Quelques questions sur notre bot",
-                    "image_url": "https://raw.githubusercontent.com/mlaidouni/FacebookBot/master/facebook-chatbots.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "C’est quoi un bot Facebook?",
-                        "payload": "Un bot conversationnel, c’est un logiciel informatique qui réalise des conversations automatisées. Un bot Facebook, c’est la même chose, mais sur un certain réseau social",
-                    },{
-                        "type": "postback",
-                        "title": "Quelles sont les entreprises qui ont essayé les bots ?",
-                        "payload": "Il s’agit de CNN, qui vous envoie un briefing par message chaque matin, comme une newsletter, quoi. En France, il y a notamment Skyscanner, Voyages-Sncf et KLM.",
-                    }, {
-                        "type": "postback",
-                        "title": "Comment créer votre bot Facebook?",
-                        "payload": "Il existe deux façons pour créer un bot facebook, soit d'utiliser une plateforme sans codage ou le créer en codage",
-                    }],
-                },  {
-                    "title": "Les outils nécessaires",
-                    "subtitle": "pour créer Facebook Messenger Bot",
-                    "image_url": "https://raw.githubusercontent.com/mlaidouni/FacebookBot/master/Facebook%20Messanger%20Bot.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "C'est quoi Node.js",
-                        "payload": "Node.js est une plateforme de développement Javascript. Ce n'est ni un serveur ,ni un Framework, c'est juste le langage Javascript avec des bibliothèques permettant de réaliser des actions comme écrire sur la sortie standard, ouvrir/fermer des connections réseaux ou encore créer un fichier.",
-                    },{
-                        "type": "postback",
-                        "title": "C'est quoi Heroku",
-                        "payload": "Heroku est une plateforme « Cloud » qui permet d’héberger, de développer tout type d’application. La plateforme appartient à Salesforce, gage de sécurité, de confidentialité et de haute performance.",
-                    }, {
-                        "type": "postback",
-                        "title": "C'est quoi GitHub",
-                        "payload": "GitHub est un site où n’importe qui peut déposer son projet web. Qu'il s'agit de quelques lignes de code pour une page modeste, ou d’une grosse application, les amateurs et professionnels ouvrent des comptes sur GitHub pour soumettre leur travail à l’appréciation de tous.",
-                    }],
-                }]  
-            } 
-        }
+function SendMessage(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+ console.log("Log X :"+JSON.stringify(request_body)+"-----"+response);
+  
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": token },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!-->'+JSON.stringify(request_body))
+    } else {
+      console.error("Unable to send message:" + err);
     }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+  }); 
 }
 
+
+
+function getAiml(request) {
+  let  roboResponse='Hi' ;
+  var aimlInterpreter = new AIMLInterpreter({name:'taha', age:'1 month', ufirst: 'Takoua', ulast: 'Kharroubi', gender: 'Female'});
+  aimlInterpreter.loadAIMLFilesIntoArray(["responses/bot.aiml"]);
+  aimlInterpreter.findAnswerInLoadedAIMLFiles(request.toUpperCase(), function(answer, wildCardArray, input){
+    if(answer){
+       roboResponse = answer;
+    }else{
+      roboResponse = "hmmm... I'm not sure what to say about that. Try saying help to see some options.";
+    }
+  });
+  // Save user input and aiml output to dynamoDb
+ // console.log(request.sender);
+  //console.log(request.text + request.sender+ request.timestamp, roboResponse);
+  return roboResponse;
+}
+
+
+
+
+
+function sendTextMessage(sender, text) { 
+ let messageData = { text:text } 
+ request({  
+  url: 'https://graph.facebook.com/v2.6/me/messages',  
+  qs: {access_token:token},  
+  method: 'POST',  
+  json: {   recipient: {id:sender},   message: messageData,
+        } 
+ }, function(error, response, body) { 
+  if (error) {  
+   console.log('Error sending messages: ', error)  
+  } else if (response.body.error) { 
+   console.log('Error: ', response.body.error)  
+  } 
+ })
+}
